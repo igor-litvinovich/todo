d1456b50aa15963a2c1be4812c7a1d03a339c9e9
IgorLit <igorek96by@gmail.com>
2017-06-10 10:52:58 +0300

    initial commit

---
+++ app/navModel.js
@@ -0,0 +1,25 @@
+class NavModel {
+    constructor() {
+        this.links = [
+            { title: 'All' },
+            { title: 'Active' },
+            { title: 'Completed' }
+        ];
+
+        this.active = this.links[0];
+    }
+
+    getLinks() {
+        return this.links;
+    }
+
+    getActive() {
+        return this.active;
+    }
+
+    setActive(link) {
+        this.active = link;
+    }
+}
+
+module.exports = NavModel;
